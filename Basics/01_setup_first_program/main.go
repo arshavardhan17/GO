89b28c40ec363c09cd1423d6997fc02754d98d90
arshavardhan17 <2300031000@kluniversity.in>
2026-01-14 22:58:08 +0530

    first commit

---
+++ Basics/01_setup_first_program/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// main is the entry file
+func main(){
+	fmt.Println("Hello go")
+}
---
+++ Basics/02_variables/main.go
@@ -0,0 +1,15 @@
+
+// var name type
+
+
+package main
+
+import "fmt"
+
+func main(){
+	var name string ="arsha"
+	var age int =20
+
+	fmt.Println("Name",name)
+	fmt.Println("age",age)
+}
