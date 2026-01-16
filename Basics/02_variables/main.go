58082db2bc4f52d6433e307bb64ee7ad345495fe
arshavardhan17 <2300031000@kluniversity.in>
2026-01-16 21:01:11 +0530

    did some practice

--- Basics/02_variables/main.go
+++ Basics/02_variables/main.go
@@ -1,15 +1,29 @@
-
 // var name type
-
 
 package main
 
 import "fmt"
 
 func main(){
-	var name string ="arsha"
+	// long Syntax
+	var name string
+	name="Arsha"
 	var age int =20
+	var isabove18 bool = true
+
+	//short
+	var name2 ="Arsha"
+	name3:="vishnu"
 
 	fmt.Println("Name",name)
 	fmt.Println("age",age)
+	fmt.Println("isabove 18",isabove18)
+	fmt.Println("Name1",name2)
+	fmt.Println("Name1",name3)
 }
+
+// total syntax
+
+// var name string ="arsha"
+// var name2 ="Arsha"
+// name3:="vishnu"
