d428e66176a0a15bbfd402e2ce2cc1dec4c53017
arshavardhan17 <2300031000@kluniversity.in>
2026-01-18 16:40:15 +0530

    addes maps

---
+++ Basics/10_maps/maps.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+//maps -> hashmap,objects,dicteronires etc
+func main(){
+// crearing a map
+m:=make(map[string]string)
+
+//setting elements
+m["name"]="golang"
+m["area"]="backend"
+//geting an element
+fmt.Println(m["name"],m["area"])
+fmt.Println(m)
+fmt.Println(len(m))
+
+// deleting
+delete(m,"area")
+fmt.Println(m)
+
+// clear map
+// clear(m)
+// fmt.Println(m)
+}
